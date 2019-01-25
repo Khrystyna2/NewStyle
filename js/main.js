b3571cef7d52a994f79678e48a9baa14c90ab976
Khrystyna2 <l.xrustja@gmail.com>
2019-01-25 17:57:00 +0200

    added new file and header

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
 
+    $('.menu__item>span').click(function() {
+        $(this).next('ul').slideToggle();
+        $(this).closest('li').siblings('li').find('ul').slideUp();        
+    });
+
+    $('.slider-header').slick({
+        // autoplay: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true
+      });
 });
